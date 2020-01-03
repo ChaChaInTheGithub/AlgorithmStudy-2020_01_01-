d0988d7026735ee40351f0011eb33baf979bd571
ChaChaInTheGithub <58865827+ChaChaInTheGithub@users.noreply.github.com>
2020-01-03 23:37:27 +0900

    2020 01 03

---
+++ main.java
@@ -0,0 +1,11 @@
+package baekjoon;
+
+public class main{
+
+    public static void main(String []args){
+       System.out.println("\\    /\\");
+       System.out.println(" )  ( ')");
+       System.out.println("(  /  )");
+       System.out.println(" \\(__)|");
+    }
+}
